eda7c300cddf6ae295ae4ce23f44e66d46cfc87f
Wallace de Souza <wallace.silvadesouza67@gmail.com>
2022-05-30 07:46:52 -0300

    add file of routes at project

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello type" });
-});
-
+app.use(categoriesRoutes);
 app.listen(3333, () => {
     console.log("Server running");
 });
